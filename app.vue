ef051507243c164e71fee08d4104e48fa9649264
Elias Witt <wittbrass@icloud.com>
2023-03-19 22:54:54 -0400

    bare bones

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage/>
+    <nav style="display:flex; gap:10px">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="groups">Groups</NuxtLink>
+    <NuxtLink to="events">Events</NuxtLink>
+    <NuxtLink to="search">Search</NuxtLink>
+    </nav>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>Vintage 2023</h1>
+</template>
+
+<style scoped></style>
