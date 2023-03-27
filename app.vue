634a168f6d6ae1b956de8de9ab09175f9b7729ac
Elias Witt <wittbrass@icloud.com>
2023-03-26 20:54:21 -0400

    created the layouts folder

--- app.vue
+++ app.vue
@@ -1,13 +1,9 @@
 <template>
-    <NuxtPage/>
-    <nav class="nav">
-        <NuxtLink to="events">Events</NuxtLink>
-        <NuxtLink to="groups">Groups</NuxtLink>
-        <NuxtLink to="search">Search</NuxtLink>
-    </nav>
-
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <style lang="scss">
-@import '~/scss/styles.scss', '~/scss/fonts.scss' ;
+@import '~/scss/styles.scss', '~/scss/fonts.scss';
 </style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <nav class="nav">
+        <NuxtLink to="events">Events</NuxtLink>
+        <NuxtLink to="groups">Groups</NuxtLink>
+        <NuxtLink to="search">Search</NuxtLink>
+      </nav>
+    </header>
+
+    <div>
+        <slot />
+    </div>
+  </div>
+</template>
