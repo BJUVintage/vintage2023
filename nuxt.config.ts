b7ec9c74a07af7bc26090f82140d3a7a56a51c5a
realwitt <wittbrass@icloud.com>
2023-04-29 14:37:31 -0400

    typo: reee camelcase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: 'VINTAGE 2023',
+      title: 'Vintage 2023',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
